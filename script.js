ce4aea700da1064ef664b4401c2bc55c02293fc9
Vinit Shahdeo <vinit.shahdeo@postman.com>
2020-11-09 01:25:32 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+(function ($) {
+    "use strict";
+      $('.sakura-falling').sakura();
+})(jQuery);
+
+// $(document).ready(function() {
+//   $("#my_audio").get(0).play();
+// });
+
+// window.addEventListener("DOMContentLoaded", event => {
+//   const audio = document.querySelector("audio");
+//   audio.volume = 0.2;
+//   audio.play();
+// });
+
+
+
+// var audio = document.createElement('audio');
+// var source = document.createElement('source');
+// var media = document.getElementById('media');
+// media.appendChild(audio);
+// audio.appendChild(source);
+// source.setAttribute('src', './song.mp3');
+// source.setAttribute('type', 'audio/mpeg');
+// audio.setAttribute('autoplay', 'autoplay');
+
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 30, 2021 00:00:00").getTime();
+var countDownDate = new Date("Nov 29, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
