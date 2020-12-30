a345d8185f4affd56082adb1d95db899bad90b90
Vinit Shahdeo <vinitshahdeo@gmail.com>
2020-12-30 13:46:49 +0530

    added comments and changed the text once the timer ends

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@
       $('.sakura-falling').sakura();
 })(jQuery);
 
+/**
+ *
+ * Despite so many new Bollywood and English song options, I prefered to use two-decade-old song, Din Shagna Da!
+ *
+ * Ever attended a North Indian Wedding? As soon as the DJ plays Din Shagna Da song, it means that the much-awaited moment is here
+ * and the bride is all set to put her first foot forward to the wedding venue under a breathtaking phoolon ki chaadar.
+ * Let's keep the sky-high status of this song untouched!
+ *
+ * When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
+ * Choose a heart-touching track! 🎵 ❤️
+ *
+ * Listen here: https://youtu.be/X0MDALpV29s
+ *
+ */
 $(document).on('click', function(){
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
@@ -35,7 +49,7 @@ var x = setInterval(function() {
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "I'm a married man!";
+        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
     }
 }, 1000);
 
