b49390b4ec822117cd5e6c5c74ef69efdff878d2
Vinit Shahdeo <vinitshahdeo@gmail.com>
2023-12-02 19:29:18 +0530

    Updated the date to show the timer

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2023 00:00:00").getTime();
+var countDownDate = new Date("Nov 29, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
